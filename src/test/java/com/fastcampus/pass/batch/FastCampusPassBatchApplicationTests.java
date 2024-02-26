83e5668f688f7a6eef0b7b22f6450b843a282769
Seong-Hoon-Lim <hooney200@gmail.com>
2024-02-26 22:18:56 +0900

    first commit

---
+++ src/test/java/com/fastcampus/pass/batch/FastCampusPassBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fastcampus.pass.batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastCampusPassBatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
