cfee9ff5a8808f25866c07e0111462cd4df0d832
Seong-Hoon-Lim <hooney200@gmail.com>
2024-03-15 22:43:49 +0900

    #9 - 기존의 batch 프로젝트 생성 시 애플리케이션 실행 되지 않고 에러 발생되어 프로젝트 구조 수정
    todo: 현재 애플리케이션 실행 시 에러는 발생되지 않으나 실제 예제에 정의 해둔 job 이 제대로 실행 되지 않는 문제는 해결되지 않아 문제 해결 필요

---
+++ src/main/java/com/fastcampus/pass/config/PassJobConfig.java
@@ -0,0 +1,45 @@
+package com.fastcampus.pass.config;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableBatchProcessing
+public class PassJobConfig {
+
+    public static final String JOB_NAME = "passJob";
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Autowired
+    public PassJobConfig(JobBuilderFactory jobBuilderFactory,
+                         StepBuilderFactory stepBuilderFactory) {
+        this.jobBuilderFactory = jobBuilderFactory;
+        this.stepBuilderFactory = stepBuilderFactory;
+    }
+
+    @Bean
+    public Job passJob() {
+        return jobBuilderFactory.get(JOB_NAME)
+                .start(passStep())
+                .build();
+    }
+
+    @Bean
+    public Step passStep() {
+        return stepBuilderFactory.get("passStep")
+                .tasklet((contribution, chunkContext) -> {
+                    System.out.println("==================== execute passTasklet ====================");
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+
+}
