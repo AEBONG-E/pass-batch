a264c96e2ce60c39b1936db018c6bc630e42fecf
Seong-Hoon-Lim <hooney200@gmail.com>
2024-03-25 23:46:53 +0900

    #4 - 테스트 시 에러발생으로 주석 처리
    빌드 중 테스트 에러 발생 유발시키는 코드로 차후 조치하고 지금은 주석처리 하여 미사용

--- src/test/java/com/fastcampus/pass/batch/FastCampusPassBatchApplicationTests.java
+++ src/test/java/com/fastcampus/pass/batch/FastCampusPassBatchApplicationTests.java
@@ -3,11 +3,11 @@ package com.fastcampus.pass.batch;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class FastCampusPassBatchApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//@SpringBootTest
+//class FastCampusPassBatchApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
